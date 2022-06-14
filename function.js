0428a2a220c7d3d923c8fd396a53982061f83ae3
Rony99John <ronyjohn9870@gmail.com>
2022-06-14 23:09:58 +0300

    dd

--- function.js
+++ function.js
@@ -1,3 +1,14 @@
+class birthCard {
+  constructor(name1, name2, pass, date, g, wl) {
+    this.firstName = name1;
+    this.lastName = name2;
+    this.password = pass;
+    this.date = date;
+    this.gender = g ? "Male" : "Female";
+    this.wishList = [...wl];
+  }
+}
+
 let Cards = [
   {
     firstName: "Frodo",
@@ -26,30 +37,29 @@ let Cards = [
 ];
 
 console.log(Cards);
-let x = ``;
-let display = ``;
-for (i = 0; i < Cards.length; i++) {
-  x = `<div class="cards">
-<a>${Cards[i].firstName} ${Cards[i].lastName}</a>
-<a>${Cards[i].date}</a>
-<a>${Cards[i].gender}</a>
-<a>Edit &#128465;</a>
-</div>
-`;
-  display = display + ` ${x}`;
-}
-document.getElementById("cards").innerHTML = display;
 
-class birthCard {
-  constructor(name1, name2, pass, date, g, wl) {
-    this.firstName = name1;
-    this.lastName = name2;
-    this.password = pass;
-    this.date = date;
-    this.gender = g ? "Male" : "Female";
-    this.wishList = [...wl];
+// Function to Display the BirthDayCards depending on the given Array
+
+function displaying(array) {
+  let x = ``;
+  let display = ``;
+
+  for (i = 0; i < array.length; i++) {
+    x = `<div class="cards">
+  <a>${array[i].firstName} ${array[i].lastName}</a>
+  <a>${array[i].date}</a>
+  <a>${array[i].gender}</a>
+  <a>Edit &#128465;</a>
+  </div>
+  `;
+    display = display + ` ${x}`;
   }
+  document.getElementById("cards").innerHTML = display;
 }
+
+displaying(Cards);
+
+// Function to take inputs values to the CardsArray
 
 function AddNew() {
   let name1 = document.getElementById("fname").value;
@@ -66,22 +76,56 @@ function AddNew() {
     console.log(np);
 
     document.getElementById("form").style.display = "none";
-
-    const form = document.getElementById("form");
-
-    form.addEventListener("submit", function handleSubmit(event) {
-      event.preventDefault();
-      form.reset();
-    });
+    document.getElementById("new").style.display = "";
+    displaying(Cards);
   } else {
+    // TODO:
     console.log("complete the form");
   }
 }
 
+// Function to display or hide the input form by (Add, Cancel ) Buttons
+
 function onof() {
   if (document.getElementById("form").style.display == "") {
     document.getElementById("form").style.display = "none";
+    document.getElementById("new").style.display = "";
   } else {
     document.getElementById("form").style.display = "";
+    document.getElementById("new").style.display = "none";
   }
+}
+
+// searching by name, date, or wishlist item
+
+function search() {
+  let search = document.getElementById("srch").value.toUpperCase();
+  let srchArray = [];
+
+  //  accessing the Cards Array and taking Values from it
+
+  for (let i = 0; i < Cards.length; i++) {
+    let element = Cards[i];
+    let one = element.firstName.toUpperCase();
+    let two = element.lastName.toUpperCase();
+    let three = element.wishList;
+
+    // Checking if the First/Last Name matches with search
+
+    if (one.includes(search) || two.includes(search)) {
+      srchArray.push(element);
+    } else {
+      //  looping and searching in the wishlist
+
+      for (let z = 0; z < three.length; z++) {
+        let wish = three[z].toUpperCase();
+
+        if (wish.includes(search)) {
+          srchArray.push(element);
+          break;
+        }
+      }
+    }
+  }
+  displaying(srchArray);
 }
